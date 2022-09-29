94a3dde5a8ead9ba2f02ead2371200759a59b3a0
Md Sakil Hasan <mdsakilhasan386@gmail.com>
2022-09-29 23:15:58 +0600

    correction something

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -4,7 +4,7 @@ import './Title.css'
 const Title = () => {
     return (
         <div className='title'>
-            <h1>Bogra-Active-club</h1>
+            <h1>Bogura-Active-club</h1>
         </div>
     );
 };
