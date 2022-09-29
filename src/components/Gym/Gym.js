cc75b43bdad6bff93664f970e3752b5d48938432
Md Sakil Hasan <mdsakilhasan386@gmail.com>
2022-09-29 14:46:11 +0600

    first commit

---
+++ src/components/Gym/Gym.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Gym.css'
+
+const Gym = () => {
+    return (
+        <div className='Gym-container'>
+            <div className="activities-container">
+                <h1>I am activities</h1>
+            </div>
+            <div className="cart-container">
+                <h1>I am cart</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Gym;
